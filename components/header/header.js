24983c8a74788df370fcbe318513cd19648e50a5
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2021-11-23 18:21:44 +0200

    adjusted grid allignement .

--- components/header/header.js
+++ components/header/header.js
@@ -4,8 +4,9 @@ import styles from "./header.module.css";
 import Button from "@mui/material/Button";
 import { makeStyles } from "@mui/styles";
 import { Grid } from "@mui/material";
+import Divider from "@mui/material/Divider";
 
-const useStyles = makeStyles((them) => ({
+const useStyles = makeStyles((theme) => ({
   header: {
     backgroundColor: "red",
   },
@@ -27,94 +28,112 @@ export default function Header() {
       container
       direction="row"
       justifyContent="space-between"
-      alignItems="center"
-      spacing={3}
+      className={styles.back}
     >
-      <Grid item md={2}>
-        <Image
-          src="/Michich.png"
-          width={200}
-          height={70}
-          alt="My logo"
-          className={styles.img}
-        />
-      </Grid>
+      <Grid item container spacing={0} className={styles.back} md={7}>
+        <Grid
+          item
+          direction="row"
+          justifyContent="space-between"
+          md={4}
+          backgroundColor="red"
+        >
+          <Image
+            src="/Michich.png"
+            width={200}
+            height={70}
+            alt="My logo"
+            className={styles.img}
+          />
+        </Grid>
 
+        <Grid
+          container
+          item
+          md={8}
+          direction="row"
+          justifyContent="flex-start"
+          alignItems="center"
+          spacing={0}
+          className={styles.backgroundColor}
+        >
+          <Button variant="text" className={styles.pageButton}>
+            Home
+          </Button>
+          <Button variant="text" className={styles.pageButton}>
+            Services
+          </Button>
+          <Button variant="text" className={styles.pageButton}>
+            About
+          </Button>
+          <Button variant="text" className={styles.pageButton}>
+            Projects
+          </Button>
+          <Button variant="text" className={styles.pageButton}>
+            Skills
+          </Button>
+          <Button variant="text" className={styles.pageButton}>
+            Contacts
+          </Button>
+          <Button variant="text" className={styles.pageButton}>
+            Pages
+          </Button>
+        </Grid>
+      </Grid>
       <Grid
         item
-        md={6}
-        direction="row"
-        justifyContent="space-between"
-        alignItems="center"
-        spacing={0}
-        className={styles.backgroundColor}
-      >
-        <Button variant="text" className={styles.pageButton}>
-          Home
-        </Button>
-        <Button variant="text" className={styles.pageButton}>
-          Services
-        </Button>
-        <Button variant="text" className={styles.pageButton}>
-          About
-        </Button>
-        <Button variant="text" className={styles.pageButton}>
-          Projects
-        </Button>
-        <Button variant="text" className={styles.pageButton}>
-          Skills
-        </Button>
-        <Button variant="text" className={styles.pageButton}>
-          Contacts
-        </Button>
-        <Button variant="text" className={styles.pageButton}>
-          Pages
-        </Button>
-      </Grid>
-
-      <Grid
         container
-        item
-        md={1}
-        justifyContent="space-around"
-        alignItems="center"
-        spacing={2}
-      >
-        <Image
-          src="/instagram (1).png"
-          width={30}
-          height={30}
-          alt="instagram"
-          className={styles.mediaVectors}
-        />
-        <Image
-          src="/facebook (1).png"
-          width={25}
-          height={25}
-          alt="facebook"
-          className={styles.mediaVectors}
-        />
-        <Image
-          src="/linkedin-logo.png"
-          width={22}
-          height={22}
-          alt="linkedIn"
-          className={styles.mediaVectors}
-        />
-      </Grid>
-
-      <Grid
-        item
-        md={2}
-        width="auto"
         direction="row"
-        justifyContent="center"
-        alignItems="center"
-        className={styles.back}
+        md={5}
+        margin-right="0px"
+        justifyContent="flex-end"
+        backgroundColor="yellow"
       >
-        <Button variant="outlined" className={styles.contactMeButton}>
-          Contact Me
-        </Button>
+        <Grid
+          container
+          item
+          md={2}
+          direction="row"
+          justifyContent="space-around"
+          alignItems="center"
+          spacing={2}
+          className={styles.back}
+        >
+          <Image
+            src="/instagram (1).png"
+            width={30}
+            height={30}
+            alt="instagram"
+            className={styles.mediaVectors}
+          />
+          <Image
+            src="/facebook (1).png"
+            width={25}
+            height={25}
+            alt="facebook"
+            className={styles.mediaVectors}
+          />
+          <Image
+            src="/linkedin-logo.png"
+            width={22}
+            height={22}
+            alt="linkedIn"
+            className={styles.mediaVectors}
+          />
+        </Grid>
+
+        <Grid
+          container
+          item
+          md={3}
+          justifyContent="center"
+          alignItems="center"
+          className={styles.blue}
+        >
+          <Button variant="outlined" className={styles.contactMeButton}>
+            Contact Me
+          </Button>
+        </Grid>
       </Grid>
     </Grid>
   );
