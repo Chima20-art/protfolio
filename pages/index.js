4846a281666d612b3f14244792faf0127b6aacdf
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2021-11-25 01:45:41 +0200

    added background to home component

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,13 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Header from "../components/header/header.js";
+import Home from "../components/home/home.js";
 
-export default function Home() {
+export default function Homy() {
   return (
     <div className={styles.container}>
       <Header className={styles.header} />
+      <Home />
     </div>
   );
 }
