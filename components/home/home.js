d94057f19219c2f68e128a5789069d0a84e54ac1
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2021-11-26 23:37:27 +0200

    added two grids in Home page

--- components/home/home.js
+++ components/home/home.js
@@ -8,18 +8,17 @@ export default function Home() {
       direction="column"
       justifyContent="center"
       className={styles.contai}
-      backgroundColor="#ffff6f"
       md={12}
     >
       <Grid
         container
         direction="column"
-        backgroundColor="#ffff6f"
         textAlign="start"
         justifyContent="center"
         width="fit-content"
         className={styles.content}
         md={7}
+        width="600px"
       >
         <Grid item md={2} spacing={0} className={styles.title}>
           Hi! I'm Chaimae Michich -
@@ -28,18 +27,43 @@ export default function Home() {
           {" "}
           JAVASCRIPT DEVELOPER
         </Grid>
-        <Grid item md={2}>
+        <Grid item md={2} width="580px" className={styles.paragraph}>
           {" "}
-          Hi! I'm Chaimae Nimbus -
+          Product Designer and Digital Creative Director working in Design field
+          for 13 years so far, specialized in UI/UX,Branding and Digital
+          designs.
         </Grid>
-        <Grid item md={2}>
-          {" "}
-          Hi! I'm Chaimae Nimbus -
+        <Grid item container direction="row" md={2} spacing={2}>
+          <Grid
+            item
+            container
+            direction="column"
+            justifyContent="space-around"
+            md={6}
+          >
+            <Grid item md={3} className={styles.sTitle}>
+              Email:
+            </Grid>
+            <Grid item md={3}>
+              michichchaimae@gmail.com
+            </Grid>
+          </Grid>
+          <Grid
+            item
+            container
+            direction="column"
+            justifyContent="space-around"
+            md={6}
+          >
+            <Grid item md={3} className={styles.sTitle}>
+              Github:
+            </Grid>
+            <Grid item md={3}>
+              /Chima20.art
+            </Grid>
+          </Grid>
         </Grid>
-        <Grid item md={2}>
-          {" "}
-          Hi! I'm Chaimae Nimbus -
-        </Grid>
+
         <Grid item md={2}>
           {" "}
           Hi! I'm Chaimae Nimbus -
