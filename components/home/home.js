a7a6d14ae523ffaf97ab1beb154f384eaacca018
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2021-11-29 17:14:33 +0200

    added button to Home component

--- components/home/home.js
+++ components/home/home.js
@@ -1,6 +1,76 @@
 import styles from "./home.module.css";
 import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
 
+const BootstrapGreenButton = styled(Button)({
+  boxShadow: "none",
+  textTransform: "none",
+  fontSize: 14,
+  padding: "15px 15px",
+  border: "1px solid",
+  borderColor: "rgb(174, 180, 180)",
+  lineHeight: 1.5,
+  backgroundColor: "#A171FD",
+  color: "white",
+
+  fontFamily: [
+    "-apple-system",
+    "BlinkMacSystemFont",
+    '"Segoe UI"',
+    "Roboto",
+    '"Helvetica Neue"',
+    "Arial",
+    "sans-serif",
+    '"Apple Color Emoji"',
+    '"Segoe UI Emoji"',
+    '"Segoe UI Symbol"',
+  ].join(","),
+  "&:hover": {
+    backgroundColor: "283543",
+    backgroundColor: "#A171FD",
+    boxShadow: "none",
+  },
+  "&:active": {
+    borderColor: "#283543",
+    boxShadow: "none",
+  },
+});
+
+const BootstrapBlackButton = styled(Button)({
+  boxShadow: "none",
+  textTransform: "none",
+  fontSize: 14,
+  padding: "15px 15px",
+  border: "1px solid",
+  borderColor: "rgb(174, 180, 180)",
+  lineHeight: 1.5,
+  backgroundColor: "#333232",
+  color: "white",
+
+  fontFamily: [
+    "-apple-system",
+    "BlinkMacSystemFont",
+    '"Segoe UI"',
+    "Roboto",
+    '"Helvetica Neue"',
+    "Arial",
+    "sans-serif",
+    '"Apple Color Emoji"',
+    '"Segoe UI Emoji"',
+    '"Segoe UI Symbol"',
+  ].join(","),
+  "&:hover": {
+    backgroundColor: "283543",
+    backgroundColor: "#333232",
+
+    boxShadow: "none",
+  },
+  "&:active": {
+    borderColor: "#283543",
+    boxShadow: "none",
+  },
+});
 export default function Home() {
   return (
     <Grid
@@ -19,6 +89,7 @@ export default function Home() {
         className={styles.content}
         md={7}
         width="600px"
+        spacing={4}
       >
         <Grid item md={2} spacing={0} className={styles.title}>
           Hi! I'm Chaimae Michich -
@@ -44,7 +115,7 @@ export default function Home() {
             <Grid item md={3} className={styles.sTitle}>
               Email:
             </Grid>
-            <Grid item md={3}>
+            <Grid item md={3} className={styles.paragraph}>
               michichchaimae@gmail.com
             </Grid>
           </Grid>
@@ -58,15 +129,19 @@ export default function Home() {
             <Grid item md={3} className={styles.sTitle}>
               Github:
             </Grid>
-            <Grid item md={3}>
+            <Grid item md={3} className={styles.paragraph}>
               /Chima20.art
             </Grid>
           </Grid>
         </Grid>
 
-        <Grid item md={2}>
-          {" "}
-          Hi! I'm Chaimae Nimbus -
+        <Grid item container md={2} direction="row">
+          <Grid item md={6}>
+            <BootstrapGreenButton>Downald CV</BootstrapGreenButton>
+          </Grid>
+          <Grid item md={6}>
+            <BootstrapBlackButton>Downald CV</BootstrapBlackButton>
+          </Grid>
         </Grid>
       </Grid>
     </Grid>
