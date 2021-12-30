133bf61e9f0e1b376d8747cfbfc187901db0009b
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2021-12-30 23:05:26 +0200

    added subtitles to Projects component

--- components/skills/projects/projects.js
+++ components/skills/projects/projects.js
@@ -1,12 +1,28 @@
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
+import styles from "./projects.module.css";
 
 export default function Projects() {
   return (
-    <Grid>
-      <Grid container>
-        <Grid item> Projects </Grid>
-        <Grid item container></Grid>
+    <Grid item container direction="row">
+      <Grid
+        item
+        container
+        md={10}
+        padding="20px"
+        margin="auto"
+        justifyContent={"center"}
+        alignContent={"center"}
+        className={styles.title}
+      >
+        {" "}
+        Projects{" "}
+      </Grid>
+      <Grid item container md={7} margin="auto" justifyContent={"space-around"}>
+        <Grid className={styles.subTitle}>All</Grid>
+        <Grid className={styles.subTitle}>React-js</Grid>
+        <Grid className={styles.subTitle}>Nextjs</Grid>
+        <Grid className={styles.subTitle}>Javascript</Grid>
       </Grid>
     </Grid>
   );
