9a402a3a7578dd912767c2ab47b2855189eeb203
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2022-01-02 23:58:41 +0200

    primary flexBox adjustment for Talk component.

--- components/Talk /talk.js
+++ components/Talk /talk.js
@@ -2,11 +2,14 @@ import { Grid } from "@mui/material";
 import Image from "next/dist/client/image";
 import { Button } from "@mui/material";
 import { styled } from "@mui/material/styles";
+import styles from "./talk.module.css";
 
 const BootstrapGreenButton = styled(Button)({
   boxShadow: "none",
   textTransform: "none",
   fontSize: 14,
+  width: "150px",
+  height: "45px",
   padding: "15px 15px",
   border: "1px solid",
   borderColor: "rgb(174, 180, 180)",
@@ -40,7 +43,15 @@ const BootstrapGreenButton = styled(Button)({
 export default function Talk() {
   return (
     <Grid contain item md={12} backgroundColor="red" direction={"column"}>
-      <Grid item>Let's talk !</Grid>
+      <Grid
+        container
+        direction="row"
+        justifyContent={"center"}
+        item
+        className={styles.title}
+      >
+        Let's talk !
+      </Grid>
       <Grid
         item
         container
@@ -57,29 +68,43 @@ export default function Talk() {
           justifyContent={"center"}
           md={2}
         >
-          <Grid item backgroundColor="yellow">
+          <Grid item backgroundColor="yellow" className={styles.information}>
             {" "}
             Phone me :
           </Grid>
-          <Grid item> +380983571966</Grid>
+          <Grid item className={styles.information}>
+            {" "}
+            +380983571966
+          </Grid>
         </Grid>
         <Grid item container direction={"column"} md={2}>
-          <Grid item backgroundColor="yellow">
+          <Grid item backgroundColor="yellow" className={styles.information}>
             {" "}
             Mail me:
           </Grid>
-          <Grid item> michichchaimae@gmail.com</Grid>
+          <Grid item className={styles.information}>
+            {" "}
+            michichchaimae@gmail.com
+          </Grid>
         </Grid>
-        <Grid item container direction={"column"} md={2}>
-          <Grid item backgroundColor="yellow">
+        <Grid
+          item
+          container
+          direction={"column"}
+          md={2}
+          className={styles.information}
+        >
+          <Grid item backgroundColor="yellow" className={styles.information}>
             {" "}
             follow me
           </Grid>
-          <Grid item>linkedin/chaïmae-michich </Grid>
+          <Grid item className={styles.information}>
+            linkedin/chaïmae-michich{" "}
+          </Grid>
         </Grid>
       </Grid>
-      <Grid item>
-        <BootstrapGreenButton>Hire me</BootstrapGreenButton>
+      <Grid item container direction="row" justifyContent={"center"}>
+        <BootstrapGreenButton>Hire me </BootstrapGreenButton>
       </Grid>
     </Grid>
   );
