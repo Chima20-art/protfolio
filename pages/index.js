2d99b21f59672a1a67d955fc994fbbd60c6ad8ca
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2022-06-27 15:27:22 +0100

    added react page scroller to index.js.

--- pages/index.js
+++ pages/index.js
@@ -8,16 +8,44 @@ import About from "./about";
 import Skills from "./skills";
 import Projects from "./projects";
 import Talk from "./talk";
+import ReactPageScroller from '@yunoek/react-page-scroller';
+import { useState } from "react";
+
+
 
 export default function Homy() {
+  const[currentPage, setCurrentPage] = useState(0);
+  const [hideHeader,setHideHeader] = useState(true)
+
+  const onBeforePageScroll = (number)=>{
+    if(number == 0){
+      setHideHeader(true)
+    }else{
+      setHideHeader(false)
+    }
+    setCurrentPage(number)
+    console.log('number ',number)
+  }
+
+  const pageOnChange = (number)=>{
+    //console.log('got ',number)
+    //setCurrentPage(number)
+    //console.log('current page is ',number)
+  }
+
   return (
     <div>
+     
+     {!hideHeader && <Header Class={styles.header}  /> }
+     
+      <ReactPageScroller renderAllPagesOnFirstRender={true} pageOnChange={pageOnChange} onBeforePageScroll={onBeforePageScroll} customPageNumber={currentPage} >
       <Home />
-      <Header Class={styles.header} />
       <About ID="about" />
       <Skills ID="skills" />
       <Projects ID="projects" />
       <Talk ID="talk" />
+      </ReactPageScroller>
+      
     </div>
   );
 }
