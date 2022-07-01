ded2ea2863bb7cfe64d8e0c211c6b87d4eaaf044
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2022-07-01 22:14:04 +0100

    added Ract carousel with props.

--- pages/projects.js
+++ pages/projects.js
@@ -7,18 +7,24 @@ import Tab from '@mui/material/Tab';
 import Image from 'next/dist/client/image';
 import { useState } from 'react';
 import { Button, Typography, Modal } from '@mui/material';
+import ImageGallery from 'react-image-gallery';
 
 export default function Projects(props) {
   const projects = [
     {
       title: 'Nordic rose',
-      stack: 'Nextjs/Sanity',
+      stack: 'Next/JS',
       category: 'Management of an online blog',
       githubURl: 'https://github.com/Chima20-art/blog-Next',
       description:
         'MyStand is a crowd-funding, media sharing website, that has you donating actions instead of money out of your pocket. Single page App built with Node.js on Sails and Angular 2.0. Features social media sharing and large scale crowd-funding.',
       mainImg: '/Nordic-rose.png',
-      screenShots: [],
+      screenShots: [
+        '/Nordic-rose.png',
+        '/Nordic-rose.png',
+        '/Nordic-rose.png',
+        '/Nordic-rose.png',
+      ],
     },
     {
       title: 'Pomodoro',
@@ -27,8 +33,69 @@ export default function Projects(props) {
       githubURl: 'https://github.com/Chima20-art/pomodoro',
       description:
         'MyStand is a crowd-funding, media sharing website, that has you donating actions instead of money out of your pocket. Single page App built with Node.js on Sails and Angular 2.0. Features social media sharing and large scale crowd-funding.',
-      mainImg: '/Nordic-rose.png',
-      screenShots: [],
+      mainImg: '/pomodoro.png',
+      screenShots: [
+        '/Nordic-rose.png',
+        '/Nordic-rose.png',
+        '/Nordic-rose.png',
+        '/Nordic-rose.png',
+      ],
+    },
+    {
+      title: 'landing Page',
+      stack: 'React/JS',
+      category: 'Landing page website',
+      githubURl: 'https://github.com/Chima20-art/landing-page',
+      description:
+        'MyStand is a crowd-funding, media sharing website, that has you donating actions instead of money out of your pocket. Single page App built with Node.js on Sails and Angular 2.0. Features social media sharing and large scale crowd-funding.',
+      mainImg: '/landing-page.png',
+      screenShots: [
+        '/Nordic-rose.png',
+        '/Nordic-rose.png',
+        '/Nordic-rose.png',
+        '/Nordic-rose.png',
+      ],
+    },
+    {
+      title: 'DSI user interface',
+      stack: 'React/JS',
+      category: 'DSI website',
+      githubURl: 'https://github.com/Chima20-art/community',
+      description:
+        'MyStand is a crowd-funding, media sharing website, that has you donating actions instead of money out of your pocket. Single page App built with Node.js on Sails and Angular 2.0. Features social media sharing and large scale crowd-funding.',
+      mainImg: '/DSI.png',
+      screenShots: [
+        '/Nordic-rose.png',
+        '/Nordic-rose.png',
+        '/Nordic-rose.png',
+        '/Nordic-rose.png',
+      ],
+    },
+    {
+      title: 'Etch a Sketch',
+      stack: 'JS',
+      category: 'etch a sketch game',
+      githubURl: 'https://github.com/Chima20-art/etch-a-sketch',
+      description:
+        'MyStand is a crowd-funding, media sharing website, that has you donating actions instead of money out of your pocket. Single page App built with Node.js on Sails and Angular 2.0. Features social media sharing and large scale crowd-funding.',
+      mainImg: '/etch-a-sketch.png',
+      screenShots: [
+        '/Nordic-rose.png',
+        '/Nordic-rose.png',
+        '/Nordic-rose.png',
+        '/Nordic-rose.png',
+      ],
+    },
+  ];
+  const images = [
+    {
+      original: 'https://picsum.photos/id/1018/1000/600/',
+    },
+    {
+      original: 'https://picsum.photos/id/1015/1000/600/',
+    },
+    {
+      original: 'https://picsum.photos/id/1019/1000/600/',
     },
   ];
 
@@ -44,13 +111,14 @@ export default function Projects(props) {
     position: 'absolute',
     marginTop: '20%',
     left: '50%',
-    top: '50px',
+    top: '15%',
     transform: 'translate(-50%, -50%)',
     width: '47vw',
-    height: '100vh',
-    bgcolor: 'background.paper',
+    height: '95vh',
+    backgroundColor: 'white',
     border: '2px solid #000',
     boxShadow: 24,
+    p: 5,
   };
 
   const reactArr = ['/landing-page.png', '/DSI.png', '/map.png'];
@@ -58,6 +126,9 @@ export default function Projects(props) {
   const [state, setState] = useState('all');
   const [value, setValue] = React.useState(0);
   const [open, setOpen] = useState(false);
+  const [data, setData] = useState({});
+  const [title, setTitle] = useState();
+  const [mainImg, setMainImg] = useState();
   const handleOpen = () => setOpen(true);
   const handleClose = () => {
     console.log('handleClose');
@@ -67,6 +138,18 @@ export default function Projects(props) {
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+
+  const reactProjects = projects.filter(checkReact);
+  function checkReact(project) {
+    return project.stack == 'React/JS';
+  }
+  console.log(' react projects are: ', reactProjects);
+  const nextProjects = projects.filter(checkNext);
+  function checkNext(project) {
+    return project.stack == 'Next/JS';
+  }
+  console.log(' next projects are: ', nextProjects);
+
   return (
     <Grid
       item
@@ -84,15 +167,20 @@ export default function Projects(props) {
             sx={{
               position: 'relative',
               width: '100%',
-              height: '75%',
-              backgroundColor: 'red',
+              height: '55%',
             }}
           >
-            <Image src="/Nordic-rose.png" layout="fill" alt="nordic rose" />
+            <ImageGallery
+              items={images}
+              showPlayButton={false}
+              backgroundColor="red"
+              showThumbnails="false"
+              showFullscreenButton="false"
+            />
           </Grid>
           <Box sx={{ padding: '45px 95px 25px 25px' }}>
             <Typography item sx={{ fontSize: '22pt', lineHeight: '30pt' }}>
-              Nordic Rose
+              {data?.title}
             </Typography>
             <Typography
               item
@@ -106,7 +194,7 @@ export default function Projects(props) {
                 borderBottom: 'solid 1px #c0c0c0 ',
               }}
             >
-              Online blog{' '}
+              {data.category}
             </Typography>
             <Typography
               sx={{
@@ -117,8 +205,7 @@ export default function Projects(props) {
                 marginBottom: '15px',
               }}
             >
-              ChowNow is a commission-free online ordering system and food
-              ordering app helping restaurants feed their hungry customers.
+              {data.description}
             </Typography>
             <Button
               variant="outlined"
@@ -137,7 +224,6 @@ export default function Projects(props) {
                   backgroundColor: '#E31B6D',
                   color: 'white',
                 },
-
                 margin: 'auto',
               }}
             >
@@ -180,84 +266,79 @@ export default function Projects(props) {
             <Tab
               label="All"
               className={styles.subTitle}
-              onClick={() => setState('all')}
+              onClick={() => {
+                setState('all');
+              }}
             />
             <Tab
               label="React-js"
               className={styles.subTitle}
-              onClick={() => setState('react')}
+              onClick={() => setState('React/JS')}
             />
             <Tab
               label="Next-js"
               className={styles.subTitle}
-              onClick={() => setState('next')}
+              onClick={() => setState('Next/JS')}
             />
-            <Tab
-              label="Javascript"
-              className={styles.subTitle}
-              onClick={() => setState('javascript')}
-            />
+
             {console.log(state)}
           </Tabs>
         </Grid>
         <Grid item container md={10} margin="auto" z-index="-1">
-          {state == 'all' || state == 'javascript'
-            ? projects.map((project) => (
-                <Grid
-                  key={project}
-                  container
-                  item
-                  md={4}
-                  className={styles.card}
-                >
-                  <Grid className={styles.cardContent}>
-                    <Typography sx={{ color: 'black', fontSize: '30px' }}>
-                      {project.title}
-                    </Typography>
-                    <Typography sx={{ color: '#E31B6D', fontSize: '20px' }}>
-                      {project.stack}
-                    </Typography>
-                    <Button
-                      variant="outlined"
-                      onClick={handleOpen}
-                      sx={{
-                        bottom: '0px',
+          {projects
+            .filter((item) => {
+              if (state == 'all') return true;
+
+              return state == item.stack;
+            })
+            .map((project) => (
+              <Grid
+                key={project.title}
+                container
+                item
+                md={4}
+                className={styles.card}
+              >
+                <Grid className={styles.cardContent}>
+                  <Typography sx={{ color: 'black', fontSize: '30px' }}>
+                    {project.title}
+                  </Typography>
+                  <Typography sx={{ color: '#E31B6D', fontSize: '20px' }}>
+                    {project.stack}
+                  </Typography>
+                  <Button
+                    variant="outlined"
+                    onClick={() => {
+                      handleOpen();
+                      setData(project);
+                    }}
+                    sx={{
+                      bottom: '0px',
+                      border: ' 3px solid #E31B6D',
+                      color: 'black',
+                      borderRadius: '0px',
+                      '&:hover': {
                         border: ' 3px solid #E31B6D',
-                        color: 'black',
-                        borderRadius: '0px',
-                        '&:hover': {
-                          border: ' 3px solid #E31B6D',
-                          boxShadow: 'none',
-                          backgroundColor: '#E31B6D',
-                          color: 'white',
-                        },
-                        width: '200px',
-                        margin: 'auto',
-                      }}
-                    >
-                      Learn more
-                    </Button>
-                  </Grid>
-                  <Image
-                    className={styles.cardImage}
-                    src={project.mainImg}
-                    width="400px"
-                    height="300px"
-                    alt="rose"
-                  />
+                        boxShadow: 'none',
+                        backgroundColor: '#E31B6D',
+                        color: 'white',
+                      },
+                      width: '200px',
+                      margin: 'auto',
+                    }}
+                  >
+                    Learn more
+                  </Button>
                 </Grid>
-              ))
-            : state == 'react'
-            ? reactArr.map((image) => (
-                <Grid key={image} item md={4}>
-                  <Image src={image} width="400px" height="300px" alt="rose" />
-                </Grid>
-              ))
-            : nextArr.map((image) => (
-                <Grid key={image} item md={4}>
-                  <Image src={image} width="400px" height="300px" alt="rose" />
-                </Grid>
-              ))}
+                <Image
+                  className={styles.cardImage}
+                  src={project.mainImg}
+                  width="400px"
+                  height="300px"
+                  alt="rose"
+                />
+              </Grid>
+            ))}
         </Grid>
       </Grid>
     </Grid>
