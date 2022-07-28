95aebb789ef42a34855d6cb2e82f7f68f8b19e41
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2022-07-28 17:51:01 +0100

    removed scroller from  mobile version.

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,11 @@ import Projects from './projects';
 import Talk from './talk';
 import ReactPageScroller from '@yunoek/react-page-scroller';
 import { useState } from 'react';
+import { Box } from '@mui/system';
+import { NoEncryption } from '@mui/icons-material';
+import { Grid } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
 export default function Homy(props) {
   const [currentPage, setCurrentPage] = useState(0);
   const [hideHeader, setHideHeader] = useState(true);
@@ -30,26 +35,43 @@ export default function Homy(props) {
     //setCurrentPage(number)
     //console.log('current page is ',number)
   };
-
-  return (
-    <div>
-      {!hideHeader && <Header Class={styles.header} />}
-      <ReactPageScroller
-        renderAllPagesOnFirstRender={true}
-        pageOnChange={pageOnChange}
-        onBeforePageScroll={onBeforePageScroll}
-        customPageNumber={currentPage}
-      >
-        <Home
-          clicked={clicked}
-          setClicked={setClicked}
-          setCurrentPage={setCurrentPage}
-        />
-        <About ID="about" />
-        <Skills ID="skills" />
-        <Projects ID="projects" />
-        <Talk ID="talk" />
-      </ReactPageScroller>
-    </div>
-  );
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.up('lg'));
+  if (isMobile) {
+    return (
+      <div>
+        {!hideHeader && <Header Class={styles.header} />}
+        <Box>
+          <ReactPageScroller
+            renderAllPagesOnFirstRender={true}
+            pageOnChange={pageOnChange}
+            onBeforePageScroll={onBeforePageScroll}
+            customPageNumber={currentPage}
+          >
+            <Home
+              clicked={clicked}
+              setClicked={setClicked}
+              setCurrentPage={setCurrentPage}
+            />
+            <About ID="about" />
+            <Skills ID="skills" />
+            <Projects ID="projects" />
+            <Talk ID="talk" />
+          </ReactPageScroller>
+        </Box>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <Grid direction="column" width="100vw" height="fit-content">
+          <Home clicked={clicked} />
+          <About ID="about" />
+          <Skills ID="skills" />
+          <Projects ID="projects" />
+          <Talk ID="talk" />
+        </Grid>
+      </div>
+    );
+  }
 }
