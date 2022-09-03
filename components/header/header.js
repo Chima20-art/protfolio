986a5d0509c785b22dd0ce3896bb4760db321f41
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2022-09-03 22:43:32 +0100

    solved services .

--- components/header/header.js
+++ components/header/header.js
@@ -1,174 +1,174 @@
-import Image from 'next/image';
-import * as React from 'react';
-import styles from './header.module.css';
-import Button from '@mui/material/Button';
-import { makeStyles } from '@mui/styles';
-import { Grid } from '@mui/material';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Link from 'next/dist/client/link';
-import { useState } from 'react';
+import Image from 'next/image'
+import * as React from 'react'
+import styles from './header.module.css'
+import Button from '@mui/material/Button'
+import { makeStyles } from '@mui/styles'
+import { Grid } from '@mui/material'
+import { styled } from '@mui/material/styles'
+import Box from '@mui/material/Box'
+import Link from 'next/dist/client/link'
+import { useState } from 'react'
 
-import Divider from '@mui/material/Divider';
+import Divider from '@mui/material/Divider'
 
 const useStyles = makeStyles((theme) => ({
-  header: { backgroundColor: 'red' },
-}));
+    header: { backgroundColor: 'red' },
+}))
 
 const BootstrapButton = styled(Button)({
-  boxShadow: 'none',
-  textTransform: 'none',
-  fontSize: 15,
-  padding: '3px 3px',
-  border: 'none',
-  lineHeight: 1.5,
-  backgroundColor: 'none',
-  width: '100px',
-  fontFamily: [
-    '-apple-system',
-    'BlinkMacSystemFont',
-    '"Segoe UI"',
-    'Roboto',
-    '"Helvetica Neue"',
-    'Arial',
-    'sans-serif',
-    '"Apple Color Emoji"',
-    '"Segoe UI Emoji"',
-    '"Segoe UI Symbol"',
-  ].join(','),
-  '&:hover': {
-    backgroundColor: '#E0D2D4',
-    borderColor: '#E0C3C7',
     boxShadow: 'none',
-  },
-  '&:active': {
-    boxShadow: 'none',
-  },
-});
+    textTransform: 'none',
+    fontSize: 15,
+    padding: '3px 3px',
+    border: 'none',
+    lineHeight: 1.5,
+    backgroundColor: 'none',
+    width: '100px',
+    fontFamily: [
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+    ].join(','),
+    '&:hover': {
+        backgroundColor: '#E0D2D4',
+        borderColor: '#E0C3C7',
+        boxShadow: 'none',
+    },
+    '&:active': {
+        boxShadow: 'none',
+    },
+})
 
 export default function Header(props) {
-  const [isSelected, setIsSelected] = useState('');
-  const classes = useStyles();
-  const [spacing, setSpacing] = React.useState(2);
-  const [activeButton, setActiveButton] = useState('');
+    const [isSelected, setIsSelected] = useState('')
+    const classes = useStyles()
+    const [spacing, setSpacing] = React.useState(2)
+    const [activeButton, setActiveButton] = useState('')
 
-  const handleChange = (event) => {
-    setSpacing(Number(event.target.value));
-  };
-  const handleSelected = (a) => {
-    setIsSelected(a);
-  };
+    const handleChange = (event) => {
+        setSpacing(Number(event.target.value))
+    }
+    const handleSelected = (a) => {
+        setIsSelected(a)
+    }
 
-  return (
-    <Grid md={12} className={props.Class}>
-      <Grid container direction="row" height={'9vh'}>
-        <Grid
-          item
-          container
-          direction="row"
-          justifyContent="center"
-          alignContent={'center'}
-          md={2}
-        >
-          <Image
-            item
-            src="/Michich-2.png"
-            width={250}
-            height={80}
-            alt="My logo"
-            className={styles.img}
-          />
-        </Grid>
+    return (
+        <Grid md={12} className={props.Class}>
+            <Grid container direction="row" height={'9vh'}>
+                <Grid
+                    item
+                    container
+                    direction="row"
+                    justifyContent="center"
+                    alignContent={'center'}
+                    md={2}
+                >
+                    <Image
+                        item
+                        src="/Michich-2.png"
+                        width={250}
+                        height={80}
+                        alt="My logo"
+                        className={styles.img}
+                    />
+                </Grid>
 
-        <Grid
-          container
-          item
-          md={8}
-          direction="row"
-          justifyContent="flex-start"
-          alignItems="center"
-        >
-          <Link href={'/'} replace v>
-            <Grid>
-              <BootstrapButton
-                size="small"
-                variant="text"
-                className={
-                  isSelected == 'home'
-                    ? styles.selectedPageButton
-                    : styles.pageButton
-                }
-              >
-                Home
-              </BootstrapButton>
+                <Grid
+                    container
+                    item
+                    md={8}
+                    direction="row"
+                    justifyContent="flex-start"
+                    alignItems="center"
+                >
+                    <Link href={'/'} replace v>
+                        <Grid>
+                            <BootstrapButton
+                                size="small"
+                                variant="text"
+                                className={
+                                    isSelected == 'home'
+                                        ? styles.selectedPageButton
+                                        : styles.pageButton
+                                }
+                            >
+                                Home
+                            </BootstrapButton>
+                        </Grid>
+                    </Link>
+                    <Link href={'#about'} replace passHref>
+                        <Grid>
+                            <BootstrapButton
+                                size="small"
+                                variant="text"
+                                className={
+                                    isSelected == 'services'
+                                        ? styles.selectedPageButton
+                                        : styles.pageButton
+                                }
+                            >
+                                Services
+                            </BootstrapButton>
+                        </Grid>
+                    </Link>
+                    <Link href={'#skills'} replace passHref>
+                        <BootstrapButton
+                            size="small"
+                            variant="text"
+                            className={
+                                isSelected == 'skills'
+                                    ? styles.selectedPageButton
+                                    : styles.pageButton
+                            }
+                        >
+                            Skills
+                        </BootstrapButton>
+                    </Link>{' '}
+                    <BootstrapButton
+                        size="small"
+                        variant="text"
+                        className={
+                            isSelected == 'projects'
+                                ? styles.selectedPageButton
+                                : styles.pageButton
+                        }
+                    >
+                        <Link href={'#projects'}>
+                            <a>Projects</a>
+                        </Link>
+                    </BootstrapButton>
+                    <BootstrapButton
+                        size="small"
+                        variant="text"
+                        className={
+                            isSelected == 'contact'
+                                ? styles.selectedPageButton
+                                : styles.pageButton
+                        }
+                    >
+                        <Link href="#talk">
+                            <a> Contacts</a>
+                        </Link>
+                    </BootstrapButton>
+                </Grid>
+                <Link href="/talk" passHref>
+                    <Grid
+                        container
+                        item
+                        md={2}
+                        justifyContent="center"
+                        alignItems="center"
+                        className={styles.blue}
+                    ></Grid>
+                </Link>
             </Grid>
-          </Link>
-          <Link href={'#about'} replace passHref>
-            <Grid>
-              <BootstrapButton
-                size="small"
-                variant="text"
-                className={
-                  isSelected == 'services'
-                    ? styles.selectedPageButton
-                    : styles.pageButton
-                }
-              >
-                Services
-              </BootstrapButton>
-            </Grid>
-          </Link>
-          <Link href={'#skills'} replace passHref>
-            <BootstrapButton
-              size="small"
-              variant="text"
-              className={
-                isSelected == 'skills'
-                  ? styles.selectedPageButton
-                  : styles.pageButton
-              }
-            >
-              Skills
-            </BootstrapButton>
-          </Link>{' '}
-          <BootstrapButton
-            size="small"
-            variant="text"
-            className={
-              isSelected == 'projects'
-                ? styles.selectedPageButton
-                : styles.pageButton
-            }
-          >
-            <Link href={'#projects'}>
-              <a>Projects</a>
-            </Link>
-          </BootstrapButton>
-          <BootstrapButton
-            size="small"
-            variant="text"
-            className={
-              isSelected == 'contact'
-                ? styles.selectedPageButton
-                : styles.pageButton
-            }
-          >
-            <Link href="#talk">
-              <a> Contacts</a>
-            </Link>
-          </BootstrapButton>
         </Grid>
-        <Link href="/talk" passHref>
-          <Grid
-            container
-            item
-            md={2}
-            justifyContent="center"
-            alignItems="center"
-            className={styles.blue}
-          ></Grid>
-        </Link>
-      </Grid>
-    </Grid>
-  );
+    )
 }
--- pages/about.js
+++ pages/about.js
@@ -1,164 +1,183 @@
-import Grid from '@mui/material/Grid';
-import Image from 'next/image';
-import Divider from '@mui/material/Divider';
-import styles from './about.module.css';
-import Header from '../components/header/header';
-import JobCard from '../components/JobCard/JobCard';
-import { Typography } from '@mui/material';
+import Grid from '@mui/material/Grid'
+import Image from 'next/image'
+import Divider from '@mui/material/Divider'
+import styles from './about.module.css'
+import Header from '../components/header/header'
+import JobCard from '../components/JobCard/JobCard'
+import { Typography } from '@mui/material'
 
 export default function About(props) {
-  return (
-    <Grid
-      container
-      id={props.ID}
-      sx={{
-        width: '100vw',
-        height: { md: '100vh', xs: 'fit-content' },
-      }}
-      justifyContent={'flex-end'}
-    >
-      <Grid
-        sx={{ height: { md: '70vh' } }}
-        item
-        container
-        display={'flex'}
-        direction="column"
-        margin="auto"
-      >
+    return (
         <Grid
-          item
-          container
-          display={'flex'}
-          direction="row"
-          justifyContent={'center'}
-          alignItems="flex-end"
-          textAlign={'center'}
-          className={styles.mainTitle}
-          marginBottom="20px"
-        >
-          Services
-        </Grid>
-
-        <Grid
-          sx={{
-            flexDirection: { sx: 'column', md: 'row' },
-          }}
-          justifyContent={'center'}
-          item
-          container
-        >
-          <Grid
-            sx={{ display: { xs: 'none', md: 'flex' } }}
-            md="4"
-            item
             container
-            justifyContent="space-around"
-            alignItems={'center'}
-            direction="column"
-          >
-            <Grid item alignSelf={'flex-start'}>
-              <JobCard
-                title="Javascript developer"
-                description="Creative websites for you."
-              />
-            </Grid>
-            <Grid item>
-              <JobCard
-                title="UI/UX Designer"
-                description="Creative websites for you"
-              />
-            </Grid>
-            <Grid item alignSelf={'flex-end'}>
-              <JobCard
-                title="Freelancer"
-                description="Hire me now."
-                background_color="black"
-                textColor="white"
-              />
-            </Grid>
-          </Grid>
-          <Grid
+            id={props.ID}
             sx={{
-              padding: { xs: '20px', md: '100px' },
+                width: '100vw',
+                height: { md: '100vh', xs: 'fit-content' },
+                paddingTop: '80px',
             }}
-            md="5"
-            item
-            container
-            width="fit-content"
-            display="flex"
-            direction="row"
-            justifyContent={'space-between'}
-            alignItems={'center'}
-          >
-            <Divider
-              orientation="vertical"
-              variant="middle"
-              flexItem
-              md="1"
-              sx={{ display: { xs: 'none', md: 'flex' } }}
-            >
-              <div className={styles.vertical}>About me</div>
-            </Divider>
-
+            justifyContent={'flex-end'}
+        >
             <Grid
-              sx={{
-                width: 'fit-content',
-
-                width: { xs: '100vw', md: 'fit-content' },
-                padding: '20px',
-              }}
-              item
-              container
-              direction="column"
-              width={'fit-content'}
-              margin="20px"
+                sx={{
+                    height: { md: 'fit-content', xs: 'fit-content' },
+                }}
+                item
+                container
+                display={'flex'}
+                direction="column"
             >
-              <Grid
-                item
-                sx={{
-                  fontSize: { md: '36px', xs: '26px' },
-                  fontWeight: { md: '530', xs: '530' },
-                  fontFamily: 'M PLUS Code Latin, sans-serif',
-                }}
-                margin="auto"
-                marginLeft="0px"
-                marginBottom="10px"
-              >
-                Need creative Website ?
-              </Grid>
-              <Grid
-                item
-                sx={{
-                  fontSize: { md: '34px', xs: '25px' },
-                  fontWeight: { md: '530', xs: '530' },
-                  fontFamily: 'M PLUS Code Latin, sans-serif',
-                }}
-                margin="auto"
-                marginLeft="0px"
-                marginTop="0px"
-                marginBottom={'30px'}
-              >
-                I can help you{' '}
-              </Grid>
-              <Grid
-                item
-                className={styles.paragraph}
-                sx={{ width: { xs: '90vw', md: '500px' } }}
-                margin="auto"
-                marginLeft="0px"
-              >
-                As a web developer, I enjoy using my obsessive attention to
-                detail, my unequivocal love for making things, and my
-                mission-driven work ethic to literally change the world. I am
-                passionate about building excellent software that improves the
-                lives of those around me.  I have serious passion for UI
-                effects, animations and creating intuitive, dynamic user
-                experiences. Lets make something special.{' '}
-              </Grid>
+                <Grid
+                    item
+                    container
+                    display={'flex'}
+                    direction="row"
+                    justifyContent={'center'}
+                    alignItems="flex-end"
+                    textAlign={'center'}
+                    className={styles.mainTitle}
+                    marginBottom="20px"
+                >
+                    Services
+                </Grid>
+
+                <Grid
+                    sx={{
+                        flexDirection: { xs: 'column', md: 'row' },
+                        height: 'fit-content',
+                    }}
+                    justifyContent={'center'}
+                    item
+                    container
+                >
+                    <Grid
+                        sx={{
+                            display: { xs: 'none', md: 'flex' },
+
+                            height: '50vh',
+                            margin: 'auto 0px',
+                        }}
+                        md="4"
+                        item
+                        container
+                        justifyContent="space-between"
+                        alignItems={'center'}
+                        direction="column"
+                    >
+                        <Grid item alignSelf={'flex-start'}>
+                            <JobCard
+                                title="Javascript developer"
+                                description="Creative websites for you."
+                            />
+                        </Grid>
+                        <Grid item>
+                            <JobCard
+                                title="UI/UX Designer"
+                                description="Creative websites for you"
+                            />
+                        </Grid>
+                        <Grid item alignSelf={'flex-end'}>
+                            <JobCard
+                                title="Freelancer"
+                                description="Hire me now."
+                                background_color="black"
+                                textColor="white"
+                            />
+                        </Grid>
+                    </Grid>
+                    <Grid
+                        sx={{
+                            padding: { xs: '20px', md: '100px' },
+
+                            display: 'flex',
+                            flexDirection: 'row',
+                        }}
+                        md="5"
+                        item
+                        container
+                        justifyContent={'center'}
+                        alignItems={'center'}
+                    >
+                        <Divider
+                            orientation="vertical"
+                            variant="middle"
+                            flexItem
+                            md="1"
+                            sx={{
+                                flex: 1,
+                                display: { xs: 'none', md: 'none' },
+                                backgroundColor: 'yellow',
+                            }}
+                        >
+                            <div className={styles.vertical}>About me</div>
+                        </Divider>
+
+                        <Grid
+                            sx={{
+                                flex: 5,
+                                width: 'fit-content',
+
+                                width: { xs: '100vw', md: 'fit-content' },
+                                padding: '20px',
+                            }}
+                            item
+                            container
+                            direction="column"
+                            margin="20px"
+                        >
+                            <Grid
+                                item
+                                sx={{
+                                    fontSize: { md: '36px', xs: '26px' },
+                                    fontWeight: { md: '530', xs: '530' },
+                                    fontFamily: 'M PLUS Code Latin, sans-serif',
+                                }}
+                                margin="auto"
+                                marginLeft="0px"
+                                marginBottom="10px"
+                            >
+                                Need creative Website ?
+                            </Grid>
+                            <Grid
+                                item
+                                sx={{
+                                    fontSize: { md: '34px', xs: '25px' },
+                                    fontWeight: { md: '530', xs: '530' },
+                                    fontFamily: 'M PLUS Code Latin, sans-serif',
+                                }}
+                                margin="auto"
+                                marginLeft="0px"
+                                marginTop="0px"
+                                marginBottom={'30px'}
+                            >
+                                I can help you{' '}
+                            </Grid>
+                            <Grid
+                                item
+                                className={styles.paragraph}
+                                sx={{
+                                    width: {
+                                        xs: '90vw',
+                                        md: '500px',
+                                    },
+                                }}
+                                margin="auto"
+                                marginLeft="0px"
+                            >
+                                As a web developer, I enjoy using my obsessive
+                                attention to detail, my unequivocal love for
+                                making things, and my mission-driven work ethic
+                                to literally change the world. I am passionate
+                                about building excellent software that improves
+                                the lives of those around me.  I have serious
+                                passion for UI effects, animations and creating
+                                intuitive, dynamic user experiences. Lets make
+                                something special.{' '}
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
             </Grid>
-          </Grid>
         </Grid>
-      </Grid>
-    </Grid>
-  );
+    )
 }
